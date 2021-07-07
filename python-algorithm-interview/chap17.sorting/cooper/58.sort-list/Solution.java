30b8b3f842b554ae39a11b7f7b2b71b787bf1dce
pbg0205 <pbg0205@naver.com>
2021-07-07 12:02:32 +0900

    [#1][cooper] 57.sort-list(병합정렬 풀이)

---
+++ python-algorithm-interview/chap17.sorting/cooper/58.sort-list/Solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode half = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            half = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        half.next = null;
+
+        return mergeTwoLists(sortList(head), sortList(slow));
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
