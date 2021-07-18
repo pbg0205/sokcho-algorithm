f3cea188279d31e5dcdb25493429a8c64442d1bb
woody <woojihye2339@gmail.com>
2021-07-18 20:17:15 +0900

    [#2][woody] 65.binary search

---
+++ python-algorithm-interview/chap18.binarySearch/woody/65.binary-search/Solution.py
@@ -0,0 +1,21 @@
+# Runtime: 208 ms
+# Memory Usage: 14.7 MB
+
+class Solution(object):
+    def search(self, nums, target):
+
+        left, right = 0, len(nums) - 1
+
+        while(left <= right):
+            mid =  (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
