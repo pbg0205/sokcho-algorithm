6491212e81ee34e123a7d61f30caac8a943d24c8
pbg0205 <pbg0205@naver.com>
2021-08-09 03:04:15 +0900

    [#5][cooper] task-scheduler

---
+++ python-algorithm-interview/chap21.greedy-algorithm/cooper/80.task-scheduler/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if (tasks.length == 0) {
+            return 0;
+        }
+
+        Map<Character, Integer> countMap = new HashMap<>();
+        for (char task : tasks) {
+            countMap.put(task, countMap.getOrDefault(task, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(countMap.values());
+
+        int placeForIdle = pq.poll() - 1;
+        int totalIdles = placeForIdle * n;
+
+        while (!pq.isEmpty()) {
+            totalIdles -= Math.min(pq.poll(), placeForIdle);
+        }
+
+        return totalIdles > 0 ?
+                totalIdles + tasks.length : tasks.length;
+    }
+}
+
